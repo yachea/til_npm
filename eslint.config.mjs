5ad8835882e4210f373b09f70dc9a198d21079ea
yachea <dev.yachea@gmail.com>
2025-07-28 13:18:10 +0900

    [docs] Date 라이브러리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,7 @@ export default defineConfig([
       "prettier/prettier": "warn",
       "no-var": "warn",
       "no-unused-vars": "error", // ✅ 선언했지만 사용하지 않으면 에러
+      "react/react-in-jsx-scope": "off",
     },
     settings: {
       react: {
