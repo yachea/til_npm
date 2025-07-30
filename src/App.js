448def8297c6232ea4b2890311d26babd5b20294
yachea <dev.yachea@gmail.com>
2025-07-30 11:50:41 +0900

    [docs] Ant Desing의 이해

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/layout/Header";
+import JoinPage from "./pages/JoinPage";
 import Slide from "./pages/Slide";
 
 function App() {
   return (
     <div>
       <Header />
-      <Slide />
+      {/* <Slide /> */}
+      <JoinPage />
     </div>
   );
 }
---
+++ src/pages/JoinPage.jsx
@@ -0,0 +1,21 @@
+import JoinForm from "../components/form/JoinForm";
+import PwForm from "../components/form/PwForm";
+
+function JoinPage() {
+  const formWrap = {
+    margin: "0 auto",
+    width: "80%",
+    background: "#fefefe",
+  };
+  return (
+    <div style={formWrap}>
+      <h1>회원가입</h1>
+      <div>
+        {/* <JoinForm /> */}
+        <PwForm />
+      </div>
+    </div>
+  );
+}
+
+export default JoinPage;
