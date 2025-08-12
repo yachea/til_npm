a008ff93b0b2b16bc8dc61b4a4f5c47b05b60647
yachea <dev.yachea@gmail.com>
2025-08-12 12:32:01 +0900

    [docs] framer motion 기본 활용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import AddForm from "./components/editor/AddForm";
 import AniPage from "./pages/AniPage";
+import Framer from "./pages/Framer";
 
 function App() {
   return (
     <div>
-      <h1>Anime</h1>
-      <AniPage />
+      <h1>Framer Motion</h1>
+      <Framer />
     </div>
   );
 }
--- src/pages/Framer.jsx
+++ src/pages/Framer.jsx
@@ -1,17 +1,3 @@
-# Framer Motion
-
-- https://motion.dev/docs/react
-- https://examples.motion.dev/react
-- 참조: https://velog.io/@keumky1/Framer-Motion-입문하기
-- 참조: https://nykim.work/114
-- `npm install motion`
-- `npm i framer-motion`
-
-## 폴더 및 파일 구조
-
-- /src/pages/Framer.jsx
-
-```jsx
 import { motion } from "framer-motion";
 
 function Framer() {
@@ -59,4 +45,3 @@ function Framer() {
 }
 
 export default Framer;
-```
